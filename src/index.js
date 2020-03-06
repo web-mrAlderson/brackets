418732a862b32623073c24e41ff17a4e89209f33
web-mrAlderson <tima-999@inbox.ru>
2020-03-06 15:36:35 +0300

    feat: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  if (str.length % 2 !== 0) {
+    return false
+  }
+  for (i = 0; i < str.length; i++) {
+    for (j = 0; j < bracketsConfig.length; j++) {
+      let conf = arr[arr.length - 1];
+
+      if (str[i] == bracketsConfig[j][1] && arr.length != 0 && conf == bracketsConfig[j][0]) {
+        arr.pop();
+      } else if (str[i] == bracketsConfig[j][0]) {
+        arr.push(str[i]);
+      }
+    }
+  }
+  if (arr.length == 0) { 
+		return true;
+	}
+  if (arr.length != 0) {
+	 return false;
+	}
 }
